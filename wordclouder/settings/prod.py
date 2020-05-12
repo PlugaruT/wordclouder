fc61f0bf122d6e3d357247a12e47dee02180d5a1
PlugaruT <plugaru.tudor@protonmail.com>
2020-05-12 14:23:07 +0300

    Switch to debug in prod

--- wordclouder/settings/prod.py
+++ wordclouder/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 DATABASES["default"] = dj_database_url.config(
     default=os.environ.get("DATABASE_URL", None)
