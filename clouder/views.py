5e0e642a6154a22ca63fdee4cfc6669ed30bc316
PlugaruT <plugaru.tudor@protonmail.com>
2020-05-12 13:58:54 +0300

    Display image and word frequencies in the view

--- clouder/views.py
+++ clouder/views.py
@@ -1,14 +1,45 @@
+import io
+import urllib, base64
+
 from django.shortcuts import render
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 from .models import TextComment
 from .forms import TextCommentForm
 
 
+def _create_image_as_bytes(cloud: WordCloud):
+    plt.figure(figsize=(32, 18))
+    plt.imshow(cloud, interpolation="bilinear", aspect="auto")
+    fig = plt.gcf()
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png")
+    buf.seek(0)
+    return base64.b64encode(buf.read())
+
+
 def index(request):
     obj = TextCommentForm(request.POST)
-    obj.save()
+    if obj.is_valid():
+        obj.save()
+
+    text = " ".join([v["content"] for v in TextComment.objects.all().values("content")])
+
+    cloud = WordCloud(background_color="white").generate(text)
+
+    img_bytes = _create_image_as_bytes(cloud)
+
+    uri = f"data:image/png;base64,{urllib.parse.quote(img_bytes)}"
 
     cnt = TextComment.objects.count()
     return render(
-        request, "clouder/index.html", {"form": TextCommentForm(), "obj_count": cnt}
+        request,
+        "clouder/index.html",
+        {
+            "form": TextCommentForm(),
+            "obj_count": cnt,
+            "word_frequecies": cloud.words_,
+            "image": uri,
+        },
     )
